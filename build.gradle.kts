6adc807642bd6beb7eda2efbe404cff1f83b59c1
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-04-04 21:41:22 +0100

    Modified publishing logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,7 @@ allprojects {
 
     tasks {
         shadowJar {
+            exclude("META-INF/**")
             relocate("com.willfp.libreforge.loader", "com.willfp.reforges.libreforge.loader")
             relocate("com.willfp.ecomponent", "com.willfp.reforges.ecomponent")
             relocate("kotlin", "com.willfp.eco.libs.kotlin")
